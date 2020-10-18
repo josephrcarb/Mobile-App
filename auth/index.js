f81a0386d40e64f7f86475bd66cb15dc18333ad8
josephrcarb <josephrcarb@gmail.com>
2020-10-17 20:54:41 -0700

    Backend production init

---
+++ auth/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require("dotenv").config();
+
+//Express Setup
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`App listening on port: ${PORT}`));
+
+//Mongoose Setup
+mongoose.connect(
+    process.env.MONGODB_CONNECTION_STRING, 
+    {
+        useNewUrlParser: true, 
+        useUnifiedTopology: true,
+    }, 
+    (err) => {
+        if (err) throw err;
+        console.log("MongoDB connection established.")
+    }
+);
+
+//Routes Setup
+app.use("/users", require("./routes/userRouter"));
